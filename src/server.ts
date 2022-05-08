a9bbe4b53791a92fb5715ca2d8af943eccd87c12
Felipe Silva <felipe726silva@gmail.com>
2022-05-08 13:25:15 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,13 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const port = 3333;
-const server = "http://localhost";
+//const port = 3333;
+//const server = "http://localhost";
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || port, () => {
-  console.log(`Server is running ${server}:${port}|| ${process.env.PORT}`);
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`Server is running ${process.env.PORT}`);
 });
